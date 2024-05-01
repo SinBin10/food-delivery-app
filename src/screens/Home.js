5449f8904002097901679277b92a311e18d79dd7
SinBin10 <bincode10@gmail.com>
2024-05-01 23:29:27 +0530

    Done wih Navbar and Card component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
 
 const Home = () => {
   return (
     <div>
-      <div><Navbar /></div>
-      <div><div className="card" style={{"width": "18rem"}}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div></div>
-      <div><Footer /></div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Card />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
